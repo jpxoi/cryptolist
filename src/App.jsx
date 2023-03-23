49cbc101f56577ea07fd8233e6594e7c0c21b7fd
Jean Paul Fernandez <jpxoi@icloud.com>
2023-03-23 11:07:47 -0500

    new cripto list, axios import, App.css created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
+
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then((data) => {
+        setCriptos(data.data.data)
+      })
+      .catch(() => {
+        console.log('La petición falló')
+      })
+   }, [])
+
+  if (!criptos) return <span>Cargando...</span>
+
   return (
-    <h1>Hola Mundo</h1>
+    <>
+      <h1>Lista de Criptomendas</h1>
+
+      <ol>
+        {
+          criptos.map(({id, name, priceUsd, symbol}) => (
+            <>
+              <li key={id}>Name: {name} <br/> Price: {priceUsd}</li>
+              <br/>
+            </>
+          ))
+        }
+      </ol>
+    </>
   )
 }
 
